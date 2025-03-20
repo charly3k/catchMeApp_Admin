147ff52f252e449091a13f6e6b06835ee19178ce
Boogie2k <oghosabenjamin>
2025-03-20 01:33:28 +0100

    commit

--- src/networking/logout.ts
+++ src/networking/logout.ts
@@ -1,12 +1,12 @@
 import Cookies from "universal-cookie";
 
-export const logout =  () => {
-    try {
-       const cookies = new Cookies();
-         cookies.remove('authToken');
-            cookies.remove('adminID');
-            cookies.remove('adminRole');
-    } catch (error) {
-        console.log(error);
-    }
-}
+export const logout = () => {
+  try {
+    const cookies = new Cookies();
+    cookies.remove("authToken");
+    cookies.remove("adminID");
+    cookies.remove("adminRole");
+  } catch (error) {
+    console.log(error);
+  }
+};
