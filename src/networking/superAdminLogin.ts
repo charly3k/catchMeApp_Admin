d2538abc48902c9be94017754bfe032e50b2d511
Boogie2k <oghosabenjamin>
2025-03-23 16:07:05 +0100

    commit

--- src/networking/superAdminLogin.ts
+++ src/networking/superAdminLogin.ts
@@ -1,58 +1,45 @@
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 import { apiUrl } from "./apiUrl";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-export const superAdminLogin =async (email:string,password:string) => {
-    const cookies = new Cookies();
+export const superAdminLogin = async (email: string, password: string) => {
+  const cookies = new Cookies();
 
+  try {
+    const response = await fetch(`${apiUrl}/super/admin/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const result = await response.json();
 
+    console.log(result);
 
-    try {
-    
-        const response = await fetch(`${apiUrl}/super/admin/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-             
-               
-                
-            },
-            body: JSON.stringify({
-                email,
-                password
-        })
-
-        
-    })
-const result = await response.json();
-
-
-
-console.log(result)
-
-    if(!response.ok){
-        toast(result.message,{
-            autoClose: 5000,
-            hideProgressBar: false,
-        })
-        return
-
+    if (!response.ok) {
+      toast(result.message, {
+        autoClose: 5000,
+        hideProgressBar: false,
+      });
+      return;
     }
 
-      cookies.set('authToken', result.data.accessToken);
-       cookies.set('adminID', result.data.adminDetails.id)
+    cookies.set("authToken", result.data.accessToken);
+    cookies.set("adminID", result.data.adminDetails.id);
 
-       cookies.set('adminRole', result.data.adminDetails.access)
+    cookies.set("adminRole", result.data.adminDetails.access);
 
-            toast(result.message,{
-            autoClose: 5000,
-            hideProgressBar: false,
-        })
-    
-    return result
-        
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
+    toast(result.message, {
+      autoClose: 5000,
+      hideProgressBar: false,
+    });
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
